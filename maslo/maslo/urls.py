9de5f1f7edf3056332e4ac24930a02e5a615850c
tauzen <kmioduszewski@gmail.com>
2013-11-03 19:15:38 +0100

    adding landing page

--- maslo/maslo/urls.py
+++ maslo/maslo/urls.py
@@ -9,7 +9,7 @@ from contact.views import ContactFormView
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='base.html')),
+    url(r'^$', TemplateView.as_view(template_name='landing.html')),
     url(r'^gallery/$', GalleryList.as_view()),
     url(r'^gallery/(?P<slug>[A-Za-z0-9_\-]+)/$', GalleryDetail.as_view()),
     url(r'^contact/$', ContactFormView.as_view()),
